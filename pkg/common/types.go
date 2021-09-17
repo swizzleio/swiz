c67dc796d0f52ac483ccce27aa9ce8ad185236bd
Mac Stevens <techiemac@yahoo.com>
2021-09-16 20:47:21 -0500

    Added common types

---
+++ pkg/common/types.go
@@ -0,0 +1,19 @@
+package common
+
+const (
+	OsWindows = "windows"
+	OsOsx     = "darwin"
+	OsLinux   = "linux"
+)
+
+const (
+	RemoteAccessSsh = "ssh"
+	RemoteAccessRdp = "rdp"
+)
+
+const (
+	LinuxAmazon = "Amazon"
+	LinuxCentos = "Centos"
+	LinuxUbuntu = "Ubuntu"
+	LinuxOther  = "Other"
+)
