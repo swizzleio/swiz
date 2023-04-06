a5e6c84c3d7be7c3ffd0fc6519289f17d168c284
techiemac <techiemac@yahoo.com>
2023-04-05 19:30:32 -0500

    Dummy cloudformation template for testing

---
+++ test/data/cloudformation/launchstack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws cloudformation create-stack --template-body file://bootstrap.yaml --stack-name swiz-boot --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-create-complete --stack-name swiz-boot
+LAMBDA_ARN=`aws cloudformation describe-stacks --stack-name swiz-boot --query "Stacks[0].Outputs[?OutputKey=='SleepTestFunctionArn'].OutputValue" --output text`
+aws cloudformation create-stack --template-body file://sleepstack.yaml --stack-name swiz-sleep --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=SleepTestFunctionArn,ParameterValue=$LAMBDA_ARN
