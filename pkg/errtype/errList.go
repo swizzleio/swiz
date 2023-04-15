7ab5d7f4215413537a9305814d6715cfcb168a59
techiemac <techiemac@yahoo.com>
2023-04-15 17:56:11 -0500

    Added error list type

---
+++ pkg/errtype/errList.go
@@ -0,0 +1,36 @@
+package errtype
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ErrList struct {
+	Errors []error
+}
+
+func (e *ErrList) Error() string {
+	errStr := []string{}
+	for _, err := range e.Errors {
+		errStr = append(errStr, err.Error())
+	}
+
+	return fmt.Sprintf("multiple errrs: %v", strings.Join(errStr, ", "))
+}
+
+func (e *ErrList) ErrOrNil() error {
+	if (e.Errors != nil) &&
+		(len(e.Errors) > 0) {
+		return e
+	}
+
+	return nil
+}
+
+func (e *ErrList) Add(err error) {
+	if e.Errors == nil {
+		e.Errors = []error{}
+	}
+
+	e.Errors = append(e.Errors, err)
+}
