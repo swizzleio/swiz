cb2f43d8925b5851231ee147b0c65fdd09257daf
techiemac <techiemac@yahoo.com>
2023-06-03 19:27:01 -0500

    Removed comments that are no longer needed

--- internal/environment/repo/cloudformationrepo.go
+++ internal/environment/repo/cloudformationrepo.go
@@ -134,17 +134,6 @@ func (r *CloudFormationRepo) DeleteStack(ctx context.Context, name string, dryRu
 func (r *CloudFormationRepo) UpdateStack(ctx context.Context, name string, template string,
 	params map[string]string, metadata map[string]string, dryRun bool) (*model.StackInfo, error) {
 
-	// TODO: Mimic the cloudformation deploy command:
-	// https://stackoverflow.com/questions/49945531/aws-cloudformation-create-stack-vs-deploy
-	// https://www.quora.com/How-does-AWS-CloudFormation-determine-whether-to-create-new-resources-or-updating-existing-ones-when-doing-a-deploy
-	// https://blog.boltops.com/2017/04/07/a-simple-introduction-to-aws-cloudformation-part-4-change-sets-dry-run-mode/
-
-	// This will use a create-change set and then execute it.
-	// aws cloudformation create-change-set --stack-name Foobar --change-set-name cs-1  --template-body file://bootstrap.yaml --capabilities CAPABILITY_NAMED_IAM --profile myprofile
-	// aws cloudformation describe-change-set --stack-name Foobar --change-set-name cs-1 --profile myprofile
-	// aws cloudformation list-change-sets --stack-name Foobar --profile myprofile
-	// aws cloudformation execute-change-set --stack-name Foobar --change-set-name cs-1 --profile myprofile
-
 	// Get the current timestamp
 	t := time.Now()
 	timestamp := t.Format("20060102150405")
