327ada0fe92aee3c1ecdec07706742227f4d914b
techiemac <techiemac@yahoo.com>
2023-06-04 11:53:09 -0500

    Added config export

--- internal/cmd/configexport.go
+++ internal/cmd/configexport.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+)
 
 func init() {
 	addSubCommand("config", &cli.Command{
@@ -12,5 +15,20 @@ func init() {
 }
 
 func configExportCmd(ctx *cli.Context) error {
+	if appConfig == nil {
+		return fmt.Errorf("missing app config")
+	}
+
+	out, err := appConfig.GetBase64()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("This output can be used to share the app config with developers. The signature and word list are\n")
+	fmt.Printf("used to verify the integrity of the app config and crytographically the same.\n\n")
+	fmt.Printf("App config: %v\n", out.Encoded)
+	fmt.Printf("Signature: %v\n", out.Signature)
+	fmt.Printf("Word list: %v\n", out.WordList)
+
 	return nil
 }
