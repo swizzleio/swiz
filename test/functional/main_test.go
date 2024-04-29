b03a5b165e0176c28065ac9fcee80c7b0952e583
techiemac <techiemac@yahoo.com>
2024-04-28 20:04:44 -0500

    First attempt at a new way of testing the CLI

--- test/functional/main_test.go
+++ test/functional/main_test.go
@@ -3,26 +3,11 @@
 package functional
 
 import (
-	"github.com/rogpeppe/go-internal/testscript"
-	"github.com/swizzleio/swiz/cmd/cmds"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-	os.Exit(testscript.RunMain(m, map[string]func() int{
-		"swiz": cmds.Execute,
-	}))
-}
-
-func Test_Version(t *testing.T) {
-	testscript.Run(t, testscript.Params{
-		Dir: "scripts/version",
-	})
-}
-
-func Test_Config(t *testing.T) {
-	testscript.Run(t, testscript.Params{
-		Dir: "scripts/config",
-	})
+	result := m.Run()
+	os.Exit(result)
 }
---
+++ test/functional/version_test.go
@@ -0,0 +1,60 @@
+//go:build functionaltest
+
+package functional
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/swizzleio/swiz/cmd/cmds"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	// Create pipes
+	stdinR, stdinW, _ := os.Pipe()
+	stdoutR, stdoutW, _ := os.Pipe()
+
+	// Backup original stdin and stdout
+	origStdin := os.Stdin
+	origStdout := os.Stdout
+
+	// Redirect stdin and stdout
+	os.Stdin = stdinR
+	os.Stdout = stdoutW
+
+	// Prepare to capture output
+	outputC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		_, err := io.Copy(&buf, stdoutR)
+		assert.NoError(t, err)
+		outputC <- buf.String()
+	}()
+
+	// Provide input
+	input := ""
+	go func() {
+		_, err := stdinW.Write([]byte(input))
+		assert.NoError(t, err)
+		assert.NoError(t, stdinW.Close())
+	}()
+
+	os.Args = []string{"swiz", "version"}
+
+	// Run the version command
+	ret := cmds.Execute()
+	assert.Equal(t, 0, ret)
+
+	// Clean up
+	assert.NoError(t, stdoutW.Close())
+	os.Stdin = origStdin
+	os.Stdout = origStdout
+
+	// Get the captured output. Hangs here...
+	capturedOutput := <-outputC
+
+	fmt.Println("Captured Output:", capturedOutput)
+}
