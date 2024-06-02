79743a7abb04b3fe7087dbdb372b6f00ce61e278
techiemac <techiemac@yahoo.com>
2024-06-02 13:52:44 -0500

    Added tests for disabled commands

---
+++ test/functional/general_test.go
@@ -0,0 +1,25 @@
+//go:build functionaltest
+
+package functional
+
+import (
+	"github.com/swizzleio/swiz/cmd/cmds"
+	"github.com/swizzleio/swiz/test/functional/util"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigDisableCommand(t *testing.T) {
+	cmd := []string{"swiz", "config"}
+
+	_, resp := util.RunCommandWithMocks(t, cmd, nil, util.DefaultCmdTimeoutSec, false, func(mocks cmds.FixtureMocks) error {
+		homeDir, err := os.UserHomeDir()
+		assert.NoError(t, err)
+		return util.CopyFile(mocks.Fs, "./data/simplecfg/app-config.yaml", filepath.Join(homeDir, ".swiz", "app-config.yaml"))
+	})
+
+	assert.NotContains(t, resp, "generate")
+}
