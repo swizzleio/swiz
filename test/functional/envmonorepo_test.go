3776eff7c3cfb1abad28e64ccb60fb41d1dcf5b4
techiemac <techiemac@yahoo.com>
2024-08-07 09:55:51 -0400

    Moved happy path tests to mono repo tests

--- test/functional/envmonorepo_test.go
+++ test/functional/envmonorepo_test.go
@@ -15,12 +15,12 @@ import (
 	"testing"
 )
 
-func TestEnvHappyPathIntegrationSuite(t *testing.T) {
-	suite.Run(t, new(EnvHappyPathIntegrationSuite))
+func TestEnvMonoRepoIntegrationSuite(t *testing.T) {
+	suite.Run(t, new(EnvMonoRepoIntegrationSuite))
 }
 
 // EnvHappyPathIntegrationSuite test suite
-type EnvHappyPathIntegrationSuite struct {
+type EnvMonoRepoIntegrationSuite struct {
 	suite.Suite
 }
 
@@ -28,16 +28,16 @@ func bootstrapEnvIntegration(mocks cmds.FixtureMocks) error {
 	errList := errtype.ErrList{}
 	homeDir, err := os.UserHomeDir()
 	errList.Add(err)
-	errList.Add(util.CopyDir(mocks.Fs, "./data/simplecfg", filepath.Join(homeDir, "simplecfg")))
+	errList.Add(util.CopyDir(mocks.Fs, "./data/basefs/src", filepath.Join(homeDir, "src")))
 	errList.Add(util.CopyDir(mocks.Fs, "./data/basefs/swiz", filepath.Join(homeDir, ".swiz")))
 
 	return errList.ErrOrNil()
 }
 
-func (*EnvHappyPathIntegrationSuite) SetupAllSuite() {
+func (*EnvMonoRepoIntegrationSuite) SetupAllSuite() {
 }
 
-func (*EnvHappyPathIntegrationSuite) TearDownSuite() {
+func (*EnvMonoRepoIntegrationSuite) TearDownSuite() {
 	// TODO: Check to see if there are any tagged stacks, if so, delete them
 }
 
@@ -46,8 +46,8 @@ func (*EnvHappyPathIntegrationSuite) TearDownSuite() {
 // when you are talking E2E suites, this begins to become problematic, especially when code structure is considered.
 // It's very easy for an E2E scenario to completely turn into a big ball of mud. The answer here may be more of a framework
 // that allows for E2E tests in a single test but this may also introduce long test times.
-func (s *EnvHappyPathIntegrationSuite) TestEnvAADeploy() {
-	cmd := []string{"swiz", "env", "deploy", "--name", "AwesomeEnv"}
+func (s *EnvMonoRepoIntegrationSuite) TestEnvAADeploy() {
+	cmd := []string{"swiz", "--appconfig", "file://~/src/monorepo/app-config.yaml", "env", "deploy", "--name", "AwesomeEnv"}
 
 	_, resp := util.RunCommandWithMocks(s.T(), cmd, nil, 300, false, bootstrapEnvIntegration)
 
@@ -58,8 +58,8 @@ func (s *EnvHappyPathIntegrationSuite) TestEnvAADeploy() {
 	assert.Equal(s.T(), "Stack: AwesomeEnv-swizsleep [Creating] - Create", lines[1])
 }
 
-func (s *EnvHappyPathIntegrationSuite) TestEnvABInfo() {
-	cmd := []string{"swiz", "env", "info", "--name", "AwesomeEnv"}
+func (s *EnvMonoRepoIntegrationSuite) TestEnvABInfo() {
+	cmd := []string{"swiz", "--appconfig", "file://~/src/monorepo/app-config.yaml", "env", "info", "--name", "AwesomeEnv"}
 
 	_, resp := util.RunCommandWithMocks(s.T(), cmd, nil, 300, false, bootstrapEnvIntegration)
 
@@ -73,8 +73,8 @@ func (s *EnvHappyPathIntegrationSuite) TestEnvABInfo() {
 	assert.Equal(s.T(), "  AwesomeEnv-swizboot [Complete]", lines[4])
 }
 
-func (s *EnvHappyPathIntegrationSuite) TestEnvACList() {
-	cmd := []string{"swiz", "env", "list"}
+func (s *EnvMonoRepoIntegrationSuite) TestEnvACList() {
+	cmd := []string{"swiz", "--appconfig", "file://~/src/monorepo/app-config.yaml", "env", "list"}
 
 	_, resp := util.RunCommandWithMocks(s.T(), cmd, nil, 300, false, bootstrapEnvIntegration)
 
@@ -84,8 +84,8 @@ func (s *EnvHappyPathIntegrationSuite) TestEnvACList() {
 	assert.Equal(s.T(), "AwesomeEnv", lines[0])
 }
 
-func (s *EnvHappyPathIntegrationSuite) TestEnvADDelete() {
-	cmd := []string{"swiz", "env", "delete", "--name", "AwesomeEnv"}
+func (s *EnvMonoRepoIntegrationSuite) TestEnvADDelete() {
+	cmd := []string{"swiz", "--appconfig", "file://~/src/monorepo/app-config.yaml", "env", "delete", "--name", "AwesomeEnv"}
 
 	_, resp := util.RunCommandWithMocks(s.T(), cmd, nil, 300, false, bootstrapEnvIntegration)
 
